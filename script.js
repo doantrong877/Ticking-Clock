2bef4aff8c69b1dff31ee6033ab2d42f38ba6348
Trong Doan <doantrong8777@gmail.com>
2022-06-25 15:09:32 -0700

    finished

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ function getSecondsSinceStartOfDay() {
       
   setInterval( function() {
       var time = getSecondsSinceStartOfDay();
-      console.log(time);
-      // your code here
+      var getSeconds = document.querySelector("#seconds");
+      var getMinutes = document.querySelector("#minutes");
+      var getHour = document.querySelector("#hour");
+
+
+      getSeconds.style.transform = "rotate("+(6 * new Date().getSeconds()+180) +"deg)";
+      getMinutes.style.transform = "rotate("+(0.1 * (time-(new Date().getHours() * 3600))+180) +"deg)";
+      getHour.style.transform = "rotate(" + ((1/600) * time +180) + "deg)";
+
+     
+      console.log(6 * new Date().getSeconds());
+      console.log(0.1 * (time/(new Date().getHours() * 3600)));
+      console.log((1/600) *time);
+      
+      console.log( " ");
   }, 1000);
   
